883f7667f495b6f1c1d497c56b527a6ff25da02b
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-20 10:34:51 +0900

    プロジェクト設定時に出来上がるひな形のアプリだとLabelの生成に関するロジックが重複してるのでそこを共通化するメソッドを実装

--- Resources/app.js
+++ Resources/app.js
@@ -1,13 +1,34 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+/**
+* クラスの説明文。
+* @class TiToDo
+* @constructor
+**/
 
-// create tab group
+function TiToDo(){
+	this.name = "TiToDo";
+};
+
+/**
+* プロジェクト設定時に出来上がるひな形のアプリだとLabelの生成に関するロジックが重複してるのでそこを共通化
+* @method createLabel
+* @param text {text型} Ti.Ui.Labelに設定したい文字を引数に渡す
+* @return {Ti.Ui.Label} 引数に渡された文字列でTi.Ui.Labelを返す
+**/
+
+TiToDo.prototype.createLabel = function(text){
+	var label = Titanium.UI.createLabel({
+		color:'#999',
+		text:text,
+		font:{fontSize:20,fontFamily:'Helvetica Neue'},
+		textAlign:'center',
+		width:'auto'
+	});
+	return label;
+
+};
+
 var tabGroup = Titanium.UI.createTabGroup();
 
-
-//
-// create base UI tab and root window
-//
 var win1 = Titanium.UI.createWindow({  
     title:'Tab 1',
     backgroundColor:'#fff'
@@ -18,19 +39,12 @@ var tab1 = Titanium.UI.createTab({
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+var titodo = new TiToDo();
 
+var label1 = titodo.createLabel('I amd window 1 Label');
+var label2 = titodo.createLabel('this is a window 2 Label');
 win1.add(label1);
 
-//
-// create controls tab and root window
-//
 var win2 = Titanium.UI.createWindow({  
     title:'Tab 2',
     backgroundColor:'#fff'
@@ -41,24 +55,12 @@ var tab2 = Titanium.UI.createTab({
     window:win2
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
 
 win2.add(label2);
 
 
-
-//
-//  add tabs
-//
 tabGroup.addTab(tab1);  
 tabGroup.addTab(tab2);  
 
-
-// open tab group
 tabGroup.open();
+
